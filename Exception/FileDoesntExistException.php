ec721f2feeb854b65d2aa55aeb58b34d7735d4d9
Tom Smith <tom.smith@clock.co.uk>
2012-03-01 14:24:56 +0000

    Adding comments to exceptions

--- Exception/FileDoesntExistException.php
+++ Exception/FileDoesntExistException.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File does not exist exception
+ *
+ * @author Tom Smith <tom.smith@clock.co.uk>
+ * @copyright Clock Limited 2012
+ * @license http://opensource.org/licenses/bsd-license.php New BSD License
+ */
 class FileDoesntExistException extends Exception {
 
 }
--- Exception/InvalidFileTypeException.php
+++ Exception/InvalidFileTypeException.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Invalid file type exception
+ *
+ * @author Tom Smith <tom.smith@clock.co.uk>
+ * @copyright Clock Limited 2012
+ * @license http://opensource.org/licenses/bsd-license.php New BSD License
+ */
 class InvalidFileTypeException extends Exception {
 
 }
